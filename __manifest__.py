08763dbe4b3db7fdbdbf716c9f08526b8782b56e
Saif Bodu <65458998+SaifBodu@users.noreply.github.com>
2023-12-09 17:08:43 +0530

    Add files via upload
    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name' : 'Hospital Management System',
+    'version' : '1.0',
+    'author' : 'Abdishakur',
+    'website' : 'www.example.com',
+    'summary' : 'Hospital Management System Module'
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
